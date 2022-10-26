97aa7949e5b8a9dc01d43d6102f6c4330e7e8a0d
diegooviedo3004 <81838575+diegooviedo3004@users.noreply.github.com>
2022-10-26 08:45:28 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# prueba_tecnica
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-tjhv77)
---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Card_joke from './components/Card_joke.jsx';
+import Button from '@mui/material/Button';
+
+function App() {
+  const [joke, setJoke] = useState('');
+
+  async function generate_random_meme() {
+    const res = await fetch('https://api.chucknorris.io/jokes/random');
+    const data = await res.json();
+    const joke_api = data['value'];
+    console.log(joke_api);
+    setJoke(joke_api);
+  }
+
+  useEffect(async () => {
+    await generate_random_meme();
+  }, []);
+
+  return (
+    <div className="App">
+      <Card_joke joke={joke} />
+      <Button
+        onClick={() => generate_random_meme()}
+        variant="contained"
+        style={{ marginTop: '10px' }}
+      >
+        Generar
+      </Button>
+    </div>
+  );
+}
+
+export default App;
