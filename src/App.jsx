bc8d52844f7ca55e07e33b6c66e8ffa4bf39d16c
diegooviedo3004 <81838575+diegooviedo3004@users.noreply.github.com>
2022-10-26 19:13:13 -0600

    3 files modified, 1 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
 import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 import Card_joke from './components/Card_joke.jsx';
+import Selection from './components/Selection.jsx';
 import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 function App() {
   const [joke, setJoke] = useState('');
-
-  useEffect(async () => {
-    await generate_random_meme();
-  }, []);
+  const [categoria, setCategoria] = useState('dev');
 
   async function generate_random_meme() {
     const res = await fetch('https://api.chucknorris.io/jokes/random');
     const data = await res.json();
     const joke_api = data['value'];
-    console.log(joke_api);
     setJoke(joke_api);
   }
+
+  async function generar_meme(categoria) {
+    const res = await fetch(
+      `https://api.chucknorris.io/jokes/random?category=${categoria}`
+    );
+    const data = await res.json();
+    const joke_api = data['value'];
+    setJoke(joke_api);
+  }
+
+  useEffect(() => {
+    (async function () {
+      try {
+        generate_random_meme();
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, []);
 
   return (
     <div className="App">
       <Card_joke joke={joke} />
-      <Button
-        onClick={() => generate_random_meme()}
-        variant="contained"
-        style={{ marginTop: '10px' }}
-      >
-        Generar
-      </Button>
+      <Stack spacing={2} style={{ marginTop: '10px' }}>
+        <Selection categoria={categoria} setCategoria={setCategoria} />
+        <Button onClick={() => generar_meme(categoria)} variant="contained">
+          Generar
+        </Button>
+        <Button
+          color="error"
+          onClick={() => generate_random_meme()}
+          variant="contained"
+        >
+          Aleatorio
+        </Button>
+      </Stack>
     </div>
   );
 }
---
+++ src/components/Selection.jsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useState, useEffect } from 'react';
+
+export default function BasicSelect({ setCategoria, categoria }) {
+  const [categorias, setCategorias] = useState([]);
+
+  const handleChange = (event) => {
+    setCategoria(event.target.value);
+  };
+
+  async function get_categories() {
+    const res = await fetch('https://api.chucknorris.io/jokes/categories');
+    const data = await res.json();
+    setCategorias(data);
+  }
+
+  useEffect(() => {
+    (async function () {
+      try {
+        get_categories();
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, []);
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Categorías</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          style={{ width: '100%' }}
+          label="Categorías"
+          value={categoria}
+          onChange={handleChange}
+        >
+          {categorias.map((element) => {
+            return <MenuItem value={element}>{element}</MenuItem>;
+          })}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
